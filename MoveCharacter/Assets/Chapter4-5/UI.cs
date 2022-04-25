dc4b3fc8e0de13e05d95a194d05d29c000f1d108
simon <dlgmlals3@gmail.com>
2022-04-25 12:13:58 +0900

    add mulplayer 3d guns

--- MoveCharacter/Assets/Chapter4-5/UI.cs
+++ MoveCharacter/Assets/Chapter4-5/UI.cs
@@ -5,19 +5,20 @@ using TMPro;
 public class UI : MonoBehaviour
 {
     // 1
-    // public GameObject textMeshObj;
+    //public GameObject textMeshObj;
+    //[SerializeField] private GameObject tezxt;
     // Start is called before the first frame update
     void Start()
     {
-        // GameObject textMeshObj = GameObject.Find("textMesh");
+        GameObject textMeshObj = GameObject.Find("textMesh");
 
-        /*Component[] cp = textMeshObj.GetComponents(typeof(Component));
+        Component[] cp = textMeshObj.GetComponents(typeof(Component));
         foreach (Component component in cp)
         {
             Debug.Log(" " + component.GetType());
-        }*/
+        }
 
-        //GetComponentInChildren<TextMeshProUGUI>().text = "gogogogo";
+        GetComponentInChildren<TextMeshProUGUI>().text = "gogogogo";
     }
 
     // Update is called once per frame
---
+++ MultiPlayerPixelGun3D/Assets/Scripts/PlayerNameInputManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+public class PlayerNameInputManager : MonoBehaviour
+{
+    public void SetPlayerName(string playername)
+    {
+
+        if (string.IsNullOrEmpty(playername))
+        {
+            Debug.Log("player nama is empty");
+            return;
+        }
+
+        PhotonNetwork.NickName = playername;
+
+
+
+
+
+    }
+}
